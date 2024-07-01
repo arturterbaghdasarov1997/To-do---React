3ea8ebe35fd6e71c8840a2b974836e9f37dea1f0
unknown <terbagdasarovartur@gmail.com>
2024-07-01 18:55:28 +0400

    Change class components to functional components

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
+import FuncTodo from './FuncTodo';
 import './App.css';
-import Task from './Task';
 
-let idCounter = 0;
-const generateId = () => idCounter++;
-
-class App extends Component {
-  state = {
-    tasks: [],
-    completedTasks: [],
-    newTask: ''
-  };
-
-  handleInputChange = (e) => {
-    this.setState({ newTask: e.target.value });
-  };
-
-  addTask = () => {
-    if (this.state.newTask.trim()) {
-      const newTask = {
-        id: generateId(),
-        text: this.state.newTask
-      };
-      this.setState((state) => ({
-        tasks: [...state.tasks, newTask],
-        newTask: ''
-      }));
-    }
-  };
-
-  markAsDone = (id) => {
-    const taskToMove = this.state.tasks.find(task => task.id === id);
-    this.setState((state) => ({
-      tasks: state.tasks.filter(task => task.id !== id),
-      completedTasks: [...state.completedTasks, taskToMove]
-    }));
-  };
-
-  markAsToDo = (id) => {
-    const taskToMove = this.state.completedTasks.find(task => task.id === id);
-    this.setState((state) => ({
-      completedTasks: state.completedTasks.filter(task => task.id !== id),
-      tasks: [...state.tasks, taskToMove]
-    }));
-  };
-
-  deleteTask = (id) => {
-    this.setState((state) => ({
-      tasks: state.tasks.filter(task => task.id !== id),
-      completedTasks: state.completedTasks.filter(task => task.id !== id)
-    }));
-  };
-
-  shouldComponentUpdate(nextProps, nextState) {
-    return (
-      nextState.tasks !== this.state.tasks ||
-      nextState.completedTasks !== this.state.completedTasks ||
-      nextState.newTask !== this.state.newTask
-    );
-  }
-
-  render() {
-    console.log("App render");
-    return (
-      <div className="app">
-        <h1>To-Do List</h1>
-        <div className="input-container">
-          <input type="text" value={this.state.newTask} onChange={this.handleInputChange} placeholder="Enter new task" />
-          <button onClick={this.addTask}>Add Task</button>
-        </div>
-        <div className="tasks-container">
-          <div className="tasks-column">
-            <h2>To-Do</h2>
-            {this.state.tasks.map((task) => (
-              <Task key={task.id} id={task.id} text={task.text} onDone={() => this.markAsDone(task.id)} onDelete={() => this.deleteTask(task.id)} />
-            ))}
-          </div>
-          <div className="tasks-column">
-            <h2>Done</h2>
-            {this.state.completedTasks.map((task) => (
-              <Task key={task.id} id={task.id} text={task.text} onToDo={() => this.markAsToDo(task.id)} onDelete={() => this.deleteTask(task.id)} completed />
-            ))}
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="app">
+      <h1>To-Do List</h1>
+      <FuncTodo />
+    </div>
+  );
+};
 
 export default App;
